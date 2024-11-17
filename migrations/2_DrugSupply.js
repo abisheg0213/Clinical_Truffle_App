325f13d375584ab121e9743d593c2c8375211a36
abisheg <abishegsridar@gmail.com>
2024-11-17 17:30:09 +0530

    web3 works

--- migrations/2_DrugSupply.js
+++ migrations/2_DrugSupply.js
@@ -1,5 +1,18 @@
 var Drugcompany = artifacts.require('./Drugcompany.sol')
-module.exports=function(deployer)
-{
-    deployer.deploy(Drugcompany)
+const Web3 = require("web3");
+const web3 = new Web3("http://localhost:8545");
+
+async function getAddress() {
+  const accounts = await web3.eth.getAccounts();
+  return accounts.slice(0, 10);
 }
+async function register_producer(instance,address){
+    const data=await instance.reg_producer(address);
+    console.log(data)
+}
+module.exports = async function (deployer) {
+    const addresses = await getAddress();
+    await deployer.deploy(Drugcompany, addresses[0]);
+    const instance = await Drugcompany.deployed();
+    await register_producer(instance, addresses[1]);
+};
