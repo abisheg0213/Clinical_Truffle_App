8b953f5fea92353ba428e7c24df2d876fcac993e
abisheg <abishegsridar@gmail.com>
2024-11-17 16:47:14 +0530

    Truffle

--- migrations/1_Clinical.js
+++ migrations/1_Clinical.js
@@ -6,9 +6,12 @@ async function getaddress() {
   const accounts = await web3.eth.getAccounts();
   return accounts.slice(0, 10);
 }
-
+async function register_FDA(instance,address){
+    console.log("FDA Authority Registered")
+    }
 module.exports = async function (deployer) {
-  deployer.deploy(ClinicalTrails);
-  address = await getaddress();
-  const instance = await MyContract.deployed();
+address = await getaddress();
+  deployer.deploy(ClinicalTrails,address[0]);
+  const instance = await ClinicalTrails.deployed();
+  await register_FDA(instance,address);
 };
