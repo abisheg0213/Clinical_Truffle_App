012926cad80b6c1e4d4b9572d07c0bda9d27512c
Monk <61575072+MonkNo1@users.noreply.github.com>
2024-11-17 13:25:04 +0530

    getaddress

--- migrations/1_Clinical.js
+++ migrations/1_Clinical.js
@@ -1,12 +1,14 @@
-var ClinicalTrails = artifacts.require('./ClinicalTrails.sol')
-module.exports=async function(deployer)
-{
-    deployer.deploy(ClinicalTrails)
-    const instance = await MyContract.deployed();
+var ClinicalTrails = artifacts.require("./ClinicalTrails.sol");
+const Web3 = require("web3");
+const web3 = new Web3("http://localhost:8545");
+
+async function getaddress() {
+  const accounts = await web3.eth.getAccounts();
+  return accounts.slice(0, 10);
 }
-async function testContract(instance){
-    try{
-      
-    }catch(error){}
-    
-}
+
+module.exports = async function (deployer) {
+  deployer.deploy(ClinicalTrails);
+  address = await getaddress();
+  const instance = await MyContract.deployed();
+};
--- test.js
+++
@@ -1,7 +0,0 @@
-const Web3 = require("web3");
-const web3 = new Web3("http://localhost:8545");
-
-web3.eth.getAccounts().then((accounts) => {
-  const selectedAddresses = accounts.slice(0, 10); 
-  console.log(selectedAddresses[0]); 
-});
