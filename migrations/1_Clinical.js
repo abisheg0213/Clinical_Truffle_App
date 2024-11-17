a6f18cf86138e4651e9e827cc754063423ae4424
Monk <61575072+MonkNo1@users.noreply.github.com>
2024-11-17 17:17:48 +0530

    truffle fixed

--- migrations/1_Clinical.js
+++ migrations/1_Clinical.js
@@ -2,16 +2,19 @@ var ClinicalTrails = artifacts.require("./ClinicalTrails.sol");
 const Web3 = require("web3");
 const web3 = new Web3("http://localhost:8545");
 
-async function getaddress() {
+async function getAddress() {
   const accounts = await web3.eth.getAccounts();
   return accounts.slice(0, 10);
 }
-async function register_FDA(instance,address){
-    console.log("FDA Authority Registered")
-    }
+
+async function register_FDA(instance, address) {
+  console.log("FDA Authority Registered");
+  // Additional registration logic goes here if needed
+}
+
 module.exports = async function (deployer) {
-address = await getaddress();
-  deployer.deploy(ClinicalTrails,address[0]);
+  const addresses = await getAddress();
+  await deployer.deploy(ClinicalTrails, addresses[0]);
   const instance = await ClinicalTrails.deployed();
-  await register_FDA(instance,address);
+  await register_FDA(instance, addresses);
 };
