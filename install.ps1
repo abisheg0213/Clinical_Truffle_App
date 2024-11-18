2177f97ae691257d399880f90420f830f1aed3fd
Monk <61575072+MonkNo1@users.noreply.github.com>
2024-11-19 01:57:33 +0530

    ps1 scripts with errors
    unable use that install.exe file

--- install.ps1
+++ install.ps1
@@ -9,12 +9,20 @@ if (-not $IsAdmin) {
     exit
 }
 
-# Install NodeJs
+# Install Node.js
+Read-Host -Prompt "get cal"
 $nodeUrl = "https://nodejs.org/dist/v20.18.0/node-v20.18.0-x64.msi"
 $nodeMsiPath = "$env:TEMP\node-v20.18.0-x64.msi"
 Invoke-WebRequest -Uri $nodeUrl -OutFile $nodeMsiPath
+
+# Install Node.js from the downloaded MSI
 Start-Process -Wait -FilePath msiexec.exe -ArgumentList "/i", $nodeMsiPath, "/norestart"
 
-# Install other Required Extensions  
+# Clean up the temporary MSI file after installation
+Remove-Item -Path $nodeMsiPath -Force
+
+# Install other required Node.js packages globally
 npm i -g ganache-cli
 npm i -g truffle
+
+Write-Host "Node.js, Ganache CLI, and Truffle have been installed successfully."
